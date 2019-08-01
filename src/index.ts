67b83ab1c4e83ae879edb9a55d19d8415a34b9b3
Damian Koper <kopernickk@gmail.com>
2019-08-01 21:28:32 +0200

    Sync

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export * from './greeter';
+import Device from 'hhapp-device-protocol/lib/Device/Device';
+let device = new Device({
+  type: 'watering',
+  servers: [
+    {
+      port: 2137,
+      host: 'localhost',
+    },
+  ],
+});
+
+device.init();
